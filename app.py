a7c4b2fb61746e8b90440eef6ce09ff5a12d1e9b
Elder Patten Ferreira <elderferreirass@gmail.com>
2021-01-01 10:03:21 -0800

    Add search function

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 # app.py
 
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
     return "Memes!"
+
+
+@app.route("/search")
+def search():
+    search_query = request.json.get('q')
+
+    return jsonify({
+        'q': search_query,
+        'results': []
+    })
